94463747263c3004665c4e2616d1d1f6b4af66b9
José Robert <robert.suporteweb@gmail.com>
2021-10-18 11:13:04 -0300

    :bug: Corrige calculo no valor das parcelas e na quantidade

--- src/main/java/br/com/money/credito/service/CalculaQuantidadeParcelaService.java
+++ src/main/java/br/com/money/credito/service/CalculaQuantidadeParcelaService.java
@@ -4,8 +4,6 @@ import br.com.money.credito.model.CreditoResponseTO;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
 
 @Service
 public class CalculaQuantidadeParcelaService {
@@ -13,7 +11,8 @@ public class CalculaQuantidadeParcelaService {
     public Integer calcularQuantidadeParcelas(CreditoResponseTO creditoResponseTO) {
         BigDecimal valorEmprestado = creditoResponseTO.getValorEmprestado();
         BigDecimal valorParcelas = creditoResponseTO.getValorParcela();
-        MathContext mathContext = new MathContext(2, RoundingMode.HALF_EVEN) ;
-        return valorEmprestado.divide(valorParcelas, mathContext).precision();
+        Integer novoValorEmprestado = valorEmprestado.toBigInteger().intValue();
+        Integer novoValorParcelas = valorParcelas.toBigInteger().intValue();
+        return novoValorEmprestado / novoValorParcelas;
     }
 }
--- src/main/java/br/com/money/credito/service/CalculaValorParcelaService.java
+++ src/main/java/br/com/money/credito/service/CalculaValorParcelaService.java
@@ -14,65 +14,66 @@ public class CalculaValorParcelaService {
 
     public void calcularValorParcela(CreditoResponseTO creditoResponseTO) {
         BigDecimal salario = creditoResponseTO.getSalario();
+        BigDecimal valorEmprestado = creditoResponseTO.getValorEmprestado();
 
         if (FaixaSalarial.isEntre1000And2000(salario)) {
-            BigDecimal valorParcela = salario.multiply(CINCO_PORCENTO);
+            BigDecimal valorParcela = valorEmprestado.multiply(CINCO_PORCENTO);
             valorParcela = valorParcela.setScale(2, RoundingMode.HALF_UP);
             creditoResponseTO.setValorParcela(valorParcela);
             return;
         }
 
         if (FaixaSalarial.isEntre2001And3000(salario)) {
-            BigDecimal valorParcela = salario.multiply(DEZ_PORCENTO);
+            BigDecimal valorParcela = valorEmprestado.multiply(DEZ_PORCENTO);
             valorParcela = valorParcela.setScale(2, RoundingMode.HALF_UP);
             creditoResponseTO.setValorParcela(valorParcela);
             return;
         }
 
         if (FaixaSalarial.isEntre3001And4000(salario)) {
-            BigDecimal valorParcela = salario.multiply(QUINZE_PORCENTO);
+            BigDecimal valorParcela = valorEmprestado.multiply(QUINZE_PORCENTO);
             valorParcela = valorParcela.setScale(2, RoundingMode.HALF_UP);
             creditoResponseTO.setValorParcela(valorParcela);
             return;
         }
 
         if (FaixaSalarial.isEntre4001And5000(salario)) {
-            BigDecimal valorParcela = salario.multiply(VINTE_PORCENTO);
+            BigDecimal valorParcela = valorEmprestado.multiply(VINTE_PORCENTO);
             valorParcela = valorParcela.setScale(2, RoundingMode.HALF_UP);
             creditoResponseTO.setValorParcela(valorParcela);
             return;
         }
 
         if (FaixaSalarial.isEntre5001And6000(salario)) {
-            BigDecimal valorParcela = salario.multiply(VINTE_CINCO_PORCENTO);
+            BigDecimal valorParcela = valorEmprestado.multiply(VINTE_CINCO_PORCENTO);
             valorParcela = valorParcela.setScale(2, RoundingMode.HALF_UP);
             creditoResponseTO.setValorParcela(valorParcela);
             return;
         }
 
         if (FaixaSalarial.isEntre6001And7000(salario)) {
-            BigDecimal valorParcela = salario.multiply(TRINTA_PORCENTO);
+            BigDecimal valorParcela = valorEmprestado.multiply(TRINTA_PORCENTO);
             valorParcela = valorParcela.setScale(2, RoundingMode.HALF_UP);
             creditoResponseTO.setValorParcela(valorParcela);
             return;
         }
 
         if (FaixaSalarial.isEntre7001And8000(salario)) {
-            BigDecimal valorParcela = salario.multiply(TRINTA_CINCO_PORCENTO);
+            BigDecimal valorParcela = valorEmprestado.multiply(TRINTA_CINCO_PORCENTO);
             valorParcela = valorParcela.setScale(2, RoundingMode.HALF_UP);
             creditoResponseTO.setValorParcela(valorParcela);
             return;
         }
 
         if (FaixaSalarial.isEntre8001And9000(salario)) {
-            BigDecimal valorParcela = salario.multiply(QUARENTA_PORCENTO);
+            BigDecimal valorParcela = valorEmprestado.multiply(QUARENTA_PORCENTO);
             valorParcela = valorParcela.setScale(2, RoundingMode.HALF_UP);
             creditoResponseTO.setValorParcela(valorParcela);
             return;
         }
 
         if (FaixaSalarial.isMaiorIgual9001(salario)) {
-            BigDecimal valorParcela = salario.multiply(QUARENTA_CINCO_PORCENTO);
+            BigDecimal valorParcela = valorEmprestado.multiply(QUARENTA_CINCO_PORCENTO);
             valorParcela = valorParcela.setScale(2, RoundingMode.HALF_UP);
             creditoResponseTO.setValorParcela(valorParcela);
         }
